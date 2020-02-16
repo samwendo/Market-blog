1a92d04cf947fe6039957de608719dc2d20eca0e
samwendo <wendosam21@gmail.com>
2020-02-16 10:48:20 +0300

    first commit

---
+++ config.py
@@ -0,0 +1,51 @@
+import os
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    SQLALCHEMY_DATABASE_URI=os.environ.get("DATABASE_URL")
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+class ProdConfig(Config):
+    '''
+    Production configuration child class
+
+    Args:
+        Config: The parent configuration class with general configuration settings
+    '''
+    pass
+
+
+class TestConfig(Config):
+    '''
+    Testing configuration child class
+
+    Args:
+        Config: The parent configuration class with general configuration settings
+    '''
+    pass
+
+
+class DevConfig(Config):
+    '''
+    Development configuration child class
+
+    Args:
+        Config: The parent configuration class with general configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://sam:12345@localhost/reblog'
+    DEBUG = True
+
+config_options = {
+    'development': DevConfig,
+    'production': ProdConfig,
+    'test': TestConfig
+}
---
+++ start.sh
@@ -0,0 +1,6 @@
+export SECRET_KEY='butterfly567'
+export MAIL_USERNAME='wendosam21@gmail.com'
+export MAIL_PASSWORD='12345'
+export DATABASE_URL='postgresql+psycopg2://sam:12345@localhost/reblog'
+
+python3.6 manage.py server
