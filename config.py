517058a233090ca169f82cc9b8bb1cf0d228b6d8
samwendo <wendosam21@gmail.com>
2020-02-16 12:02:42 +0300

    coments

--- config.py
+++ config.py
@@ -4,48 +4,46 @@ class Config:
     '''
     General configuration parent class
     '''
-    SECRET_KEY = os.environ.get("SECRET_KEY")
-    SQLALCHEMY_TRACK_MODIFICATIONS = True
-    SQLALCHEMY_DATABASE_URI=os.environ.get("DATABASE_URL")
-    UPLOADED_PHOTOS_DEST ='app/static/photos'
-    # MAIL_SERVER = 'smtp.googlemail.com'
-    # MAIL_PORT = 587
-    # MAIL_USE_TLS = True
-    # MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-    # MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    SECRET_KEY=os.environ.get('SECRET_KEY')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+    # email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    SUBJECT_PREFIX = 'New Blog'
+    SENDER_EMAIL = 'wendosam21@gmail.com'
+
+    # simple mde configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+
+    @staticmethod
+    def init_app(app):
+        pass
 
 class ProdConfig(Config):
     '''
     Production configuration child class
-
-    Args:
-        Config: The parent configuration class with general configuration settings
-    '''
-    pass
-
-
-class TestConfig(Config):
-    '''
-    Testing configuration child class
-
-    Args:
-        Config: The parent configuration class with general configuration settings
     '''
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://sam:54321@localhost/reblog_test'
 
 class DevConfig(Config):
     '''
     Development configuration child class
-
-    Args:
-        Config: The parent configuration class with general configuration settings
     '''
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://sam:12345@localhost/reblog'
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     DEBUG = True
 
 config_options = {
-    'development': DevConfig,
-    'production': ProdConfig,
-    'test': TestConfig
+'development': DevConfig,
+'production': ProdConfig,
+'test': TestConfig
 }
--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 from app import create_app, db
 from flask_script import Manager, Server
-from app.models import User
+from app.models import User, Blog
 from flask_migrate import Migrate, MigrateCommand
 
 # Creating app instance
@@ -23,3 +23,5 @@ def make_shell_context():
 
 if __name__ == '__main__':
     manager.run()
+    
+    
